610f0450b6a40547b3e91c347cd36d5cf7aeba76
belin zhao <zjj0888@gmail.com>
2022-05-19 13:57:19 +0800

    v 1.3.5

--- zj-api/src/main/java/com/zj/api/base/BaseErrorHandlerObservable.kt
+++ zj-api/src/main/java/com/zj/api/base/BaseErrorHandlerObservable.kt
@@ -1,7 +1,6 @@
 package com.zj.api.base
 
 import com.zj.api.adapt.AdapterPendingData
-import com.zj.api.eh.ErrorHandler
 import io.reactivex.Observable
 
 internal abstract class BaseErrorHandlerObservable<T, R>(protected val pendingData: AdapterPendingData<R>) : Observable<T>()
